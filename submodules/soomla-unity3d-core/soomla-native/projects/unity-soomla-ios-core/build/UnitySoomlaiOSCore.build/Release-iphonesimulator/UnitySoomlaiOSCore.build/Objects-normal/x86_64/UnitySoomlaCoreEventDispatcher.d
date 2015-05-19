13d7c84055798061583f77210b072f543737fddb
Lisa Hicks <lisa.hicks@sega.net>
2015-05-19 11:38:19 -0700

    Remove generated build files from GIT tracking and add to git ignore.

--- submodules/soomla-unity3d-core/soomla-native/projects/unity-soomla-ios-core/build/UnitySoomlaiOSCore.build/Release-iphonesimulator/UnitySoomlaiOSCore.build/Objects-normal/x86_64/UnitySoomlaCoreEventDispatcher.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/Lisa/Downloads/SOOMLA/unity3d-store/submodules/soomla-unity3d-core/soomla-native/projects/unity-soomla-ios-core/SoomlaUnity/UnitySoomlaCoreEventDispatcher.mm \
-  ../../../submodules/soomla-ios-core/SoomlaiOSCore/Soomla.h \
-  /Users/Lisa/Downloads/SOOMLA/unity3d-store/submodules/soomla-unity3d-core/soomla-native/projects/unity-soomla-ios-core/SoomlaUnity/UnitySoomlaCoreEventDispatcher.h \
-  ../../../submodules/soomla-ios-core/SoomlaiOSCore/SoomlaEventHandling.h \
-  ../../../submodules/soomla-ios-core/SoomlaiOSCore/rewards/Reward.h \
-  ../../../submodules/soomla-ios-core/SoomlaiOSCore/SoomlaEntity.h \
-  ../../../submodules/soomla-ios-core/SoomlaiOSCore/SoomlaUtils.h
